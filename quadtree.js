2523f1a9114e93b6ff33ded224a79d9ecf37bdf7
Nate Hart <hartbeatnt@gmail.com>
2018-03-28 19:37:51 -0700

    added point class, spy utility, and a bunch of tests

--- quadtree.js
+++ quadtree.js
@@ -1,8 +1,13 @@
 const Rectangle = require('./rectangle');
 
 module.exports = class Quadtree {
-  constructor(x,y) {
-    this.x = x;
-    this.y = y;
+  constructor(boundary=new Rectangle(), capacity = 10) {
+    this.boundary = boundary;
+    this.capacity = capacity;
+    this.entities = [];
+    this.ne = null;
+    this.nw = null;
+    this.sw = null;
+    this.se = null;
   }
 }
--- rectangle.js
+++ rectangle.js
@@ -1,5 +1,5 @@
 module.exports = class Rectangle {
-  constructor(x,y,w,h) {
+  constructor(x=1, y=1, w=1, h=1) {
     this.x = x;
     this.y = y;
     this.w = w;
